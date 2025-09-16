4c4c715384ddfed504d0d717c1976b00142ea43f
MagonxESP <janma.360@gmail.com>
2025-09-16 18:57:41 +0200

    chore: add license to all files

--- src/components/UploadItem.tsx
+++ src/components/UploadItem.tsx
@@ -1,3 +1,18 @@
+﻿/**
+ * Copyright 2025 Miku Push! Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import FileIcon from '@/components/FileIcon'
 import { Large, Small } from '@/components/Typography'
 import { Button } from '@/components/ui/button'
@@ -82,7 +97,7 @@ function UploadProgressBody({ item }: UploadItemProps) {
 function FinishedUploadBody({ item }: UploadItemProps) {
   return (
     <Small className="mt-[10px] line-clamp-1">
-      {formatSizeBytes(item.upload.size)} · {formatDate(item.upload.createdAt)}
+      {formatSizeBytes(item.upload.size)} Â· {formatDate(item.upload.createdAt)}
     </Small>
   )
 }
