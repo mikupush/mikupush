21ec6202a80b41a11bea1143b02f75564ebc6e40
MagonxESP <janma.360@gmail.com>
2025-09-23 19:50:22 +0200

    fix: sed -i option

--- scripts/apply-version.sh
+++ scripts/apply-version.sh
@@ -2,8 +2,8 @@
 
 VERSION=$(cat VERSION)
 
-sed -i .bak "3s/version = \".*\"/version = \"$VERSION\"/g" src-tauri/Cargo.toml
-sed -i .bak "s/\"version\": \".*\"/\"version\": \"$VERSION\"/g" src-tauri/tauri.conf.json
+sed -i.bak "3s/version = \".*\"/version = \"$VERSION\"/g" src-tauri/Cargo.toml
+sed -i.bak "s/\"version\": \".*\"/\"version\": \"$VERSION\"/g" src-tauri/tauri.conf.json
 
 rm src-tauri/Cargo.toml.bak
 rm src-tauri/tauri.conf.json.bak
