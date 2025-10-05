7b72039d8d4b6173e1acc60fb43fe6180422c5ad
MagonxESP <janma.360@gmail.com>
2025-10-05 02:00:26 +0200

    chore: use MemoryRouter instead of BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -21,14 +21,14 @@ import App from './App'
 import '@material/web/all'
 import { styles as typescaleStyles } from '@material/web/typography/md-typescale-styles.js'
 import '@/i18n'
-import { BrowserRouter } from 'react-router'
+import { MemoryRouter } from 'react-router'
 
 document.adoptedStyleSheets.push(typescaleStyles.styleSheet!)
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-    <BrowserRouter>
+    <MemoryRouter>
       <App />
-    </BrowserRouter>
+    </MemoryRouter>
 	</StrictMode>,
 )
