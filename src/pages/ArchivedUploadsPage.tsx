ed7a4c4238ad45814b3de67b020c32741b92b256
MagonxESP <janma.360@gmail.com>
2025-12-11 17:40:18 +0100

    chore: show in development message

--- src/pages/ArchivedUploadsPage.tsx
+++ src/pages/ArchivedUploadsPage.tsx
@@ -6,16 +6,18 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+import { useTranslation } from 'react-i18next'
 
 export default function ArchivedUploadsPage() {
-  return <h1>Archived Uploads Page</h1>
+  const { t } = useTranslation()
+  return <h1 className="text-center my-auto">{t('common.in_development')}</h1>
 }
