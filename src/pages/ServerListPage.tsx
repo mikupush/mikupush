c97455f5552329f0d259744b0d5826a602cc977b
MagonxESP <janma.360@gmail.com>
2026-05-26 16:03:59 +0200

    feat: add empty state and loading on servers page

--- src/pages/ServerListPage.tsx
+++ src/pages/ServerListPage.tsx
@@ -1,18 +1,26 @@
 import { Button } from '@/components/ui/button.tsx'
 import { useTranslation } from 'react-i18next'
-import { PlusIcon, SearchIcon } from 'lucide-react'
+import { OctagonXIcon, PlusIcon, SearchIcon } from 'lucide-react'
 import ServerItem from '@/components/ServerItem.tsx'
 import { useEffect, useState } from 'react'
 import { invoke } from '@tauri-apps/api/core'
 import { Server } from '@/model/server.ts'
+import toast from 'react-hot-toast'
+import { Paragraph } from '@/components/Typography.tsx'
+import LoadingSpinner from '@/components/LoadingSpinner.tsx'
 
 export default function ServerListPage() {
   const { t } = useTranslation()
   const [servers, setServers] = useState<Server[]>([])
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
+    setIsLoading(true)
+
     invoke<Server[]>('find_all_servers')
       .then(servers => setServers(servers))
+      .catch(() => toast.error(t('errors.server.fetch_list')))
+      .finally(() => setIsLoading(false))
   }, [])
 
   return (
@@ -28,13 +36,22 @@ export default function ServerListPage() {
           </Button>
         </div>
       </div>
-      <div className="flex flex-1 overflow-y-auto px-5">
-        <ul className="space-y-6 w-full max-w-3xl mx-auto pb-5">
-          {servers.map((server: Server) => (
-            <ServerItem {...server} />
-          ))}
-        </ul>
-      </div>
+      {(isLoading) ? (
+        <LoadingSpinner size={50} />
+      ) : (servers.length > 0) ? (
+        <div className="flex flex-1 overflow-y-auto px-5">
+          <ul className="space-y-6 w-full max-w-3xl mx-auto pb-5">
+            {servers.map((server: Server) => (
+              <ServerItem {...server} />
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div className="flex flex-1 flex-col justify-center items-center overflow-y-auto px-20">
+          <OctagonXIcon className="size-15" />
+          <Paragraph className="text-center">{t('server.empty')}</Paragraph>
+        </div>
+      )}
     </div>
   )
 }
